05b15be9a6afa013c63e0f7b32e5505784e037e7
naritayuuto <yuuto122narita@i.softbank.jp>
2023-04-29 13:15:10 +0900

    時間を図ることが多かったのでクラスを作りそちらに全任せします

---
+++ Assets/Script/Skill/IntervalTimer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntervalTimer
+{
+    float _interval = 1;
+    float _timer = 0;
+
+    public void Setup(float inv)
+    {
+        _interval = inv;
+    }
+
+    public bool RunTimer()
+    {
+        _timer += Time.deltaTime;
+        if (_timer >= _interval)
+        {
+            _timer -= _interval;
+            return true;
+        }
+        return false;
+    }
+}
