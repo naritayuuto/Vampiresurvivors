bb33065f775007c551a5ec6d7f6a4d668e6cbd95
naritayuuto <yuuto122narita@i.softbank.jp>
2023-04-29 13:10:51 +0900

    武器の親オブジェクトにアクセスできるようにした。オブジェクトプールで使用

--- Assets/Script/Skill/SkillManager.cs
+++ Assets/Script/Skill/SkillManager.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class SkillManager : MonoBehaviour
 {
+    static private SkillManager instance = default;
 
+    static public SkillManager Instance => instance;
+
+    [SerializeField,Tooltip("武器の親オブジェクト")]
+    Transform objectRoot = null;
+    public Transform ObjectRoot { get => objectRoot; }
     // Start is called before the first frame update
     void Start()
     {
